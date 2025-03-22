5da263cca8f44941f63583642dab835d1b786f2b
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-22 23:06:39 +0200

    build: fix docker compose and seeds

--- docker/mysql/init/2025-03-22-seed-job-posts-table.sql
+++ docker/mysql/init/2025-03-22-seed-job-posts-table.sql
@@ -1,4 +1,4 @@
 INSERT INTO job_posts (id, title, description, salary, employmentType, userId)
 VALUES
-(UUID(), 'Senior Software Engineer', 'We are looking for an experienced software engineer to join our team. Must have 5+ years experience with modern web technologies.', 85000, 'FULL_TIME', '1'),
-(UUID(), 'Junior Web Developer', 'Great opportunity for a junior developer to learn and grow. Knowledge of HTML, CSS, and JavaScript required.', 45000, 'FULL_TIME', '1');
+(UUID(), 'Senior Software Engineer', 'We are looking for an experienced software engineer to join our team. Must have 5+ years experience with modern web technologies.', 85000, 'on-site', '1'),
+(UUID(), 'Junior Web Developer', 'Great opportunity for a junior developer to learn and grow. Knowledge of HTML, CSS, and JavaScript required.', 45000, 'on-site', '1');
