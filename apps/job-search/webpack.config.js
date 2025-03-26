6b54a6dbf8f9442411e41de6148a76471e030012
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-26 11:14:37 +0200

    build: fix source maps and nx config for job-search

--- apps/job-search/webpack.config.js
+++ apps/job-search/webpack.config.js
@@ -2,8 +2,11 @@ const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
   output: {
     path: join(__dirname, 'dist'),
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]', // Ensures correct paths for debugging
   },
   plugins: [
     new NxAppWebpackPlugin({
@@ -12,7 +15,7 @@ module.exports = {
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       assets: ['./src/assets'],
-      optimization: false,
+      optimization: true,
       outputHashing: 'none',
       generatePackageJson: true,
     }),
