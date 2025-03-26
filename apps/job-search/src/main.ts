cf07be24523f2f912b9a228a8c831db0d251b59d
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-26 02:29:31 +0200

    fix: envs, jobsearch port

--- apps/job-search/src/main.ts
+++ apps/job-search/src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
     jsonDocumentUrl: '/swagger-json',
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
