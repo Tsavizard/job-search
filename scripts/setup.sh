c61d28977869b0f5e0065ac3fb4ef6c7c912225d
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-26 19:56:48 +0200

    chore: fix setup.sh nx run many

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,4 +4,4 @@ nvm install 23.8.0
 nvm use 23.8.0
 npm install
 
-npx nx run-many @karriera-test/job-board:build:production @karriera-test/job-search:build:production
+npx nx run-many -t build -p @karriera-test/job-board @karriera-test/job-search
