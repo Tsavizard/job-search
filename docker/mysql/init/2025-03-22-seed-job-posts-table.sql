a7404df6a094436d28febd5580201f9d21f22c45
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-26 21:47:49 +0200

    fix: mysql seeds

--- docker/mysql/init/2025-03-22-seed-job-posts-table.sql
+++ docker/mysql/init/2025-03-22-seed-job-posts-table.sql
@@ -1,4 +1,4 @@
 INSERT INTO job_posts (id, title, description, salary, work_model, userId)
 VALUES
-(UUID(), 'Senior Software Engineer', 'We are looking for an experienced software engineer to join our team. Must have 5+ years experience with modern web technologies.', 85000, 'on-site', '1'),
-(UUID(), 'Junior Web Developer', 'Great opportunity for a junior developer to learn and grow. Knowledge of HTML, CSS, and JavaScript required.', 45000, 'on-site', '1');
+(UUID(), 'Senior Software Engineer', 'We are looking for an experienced software engineer to join our team. Must have 5+ years experience with modern web technologies.', 85000, 'on-site', '3f2ecdea-02fe-48e1-bf7b-89ec6d028823'),
+(UUID(), 'Junior Web Developer', 'Great opportunity for a junior developer to learn and grow. Knowledge of HTML, CSS, and JavaScript required.', 45000, 'on-site', '3f2ecdea-02fe-48e1-bf7b-89ec6d028823');
