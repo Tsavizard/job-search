28d5898c1048b327c731518fbc85a43a955cd95c
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-03-27 11:38:14 +0200

    test: add more job search api tests for filtering and seeds

--- apps/job-search-e2e/src/job-search/job-search.api.spec.ts
+++ apps/job-search-e2e/src/job-search/job-search.api.spec.ts
@@ -5,6 +5,12 @@ describe('Job Posts API', () => {
     '3f2ecdea-02fe-48e1-bf7b-89ec6d028823.26dafafdd0de8ba46ebbdd02b1a67e04.7f7f9b79a8527b2b7c9a537f62258d2c4bf5583c82307b5199dd3eeabf66f7fb807b1614ed9b890d2a0130745d613f413ae7c05d5180764cc30c037789fba1e4';
 
   describe('GET /api/job-posts', () => {
+    it('should reject unauthorized requests', async () => {
+      await expect(axios.get('/api/job-posts')).rejects.toMatchObject({
+        response: { status: 401 },
+      });
+    });
+
     it('should list job posts', async () => {
       const res = await axios.get('/api/job-posts', {
         headers: { Authorization: `Bearer ${authToken}` },
@@ -13,16 +19,159 @@ describe('Job Posts API', () => {
       expect(res.status).toBe(200);
       expect(Array.isArray(res.data.data)).toBe(true);
       expect(res.data.meta).toStrictEqual({
-        total: 4,
+        total: 9,
         page: 1,
         limit: 10,
         totalPages: 1,
       });
     });
 
-    it('should reject unauthorized requests', async () => {
-      await expect(axios.get('/api/job-posts')).rejects.toMatchObject({
-        response: { status: 401 },
+    it('should list job posts paginated', async () => {
+      const res = await axios.get('/api/job-posts?limit=5&page=2', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 9,
+        page: 2,
+        limit: 5,
+        totalPages: 2,
+      });
+    });
+
+    it('should be able to search by title', async () => {
+      const res = await axios.get('/api/job-posts?search=QA', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 1,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should be able to search by description', async () => {
+      const res = await axios.get('/api/job-posts?search=position+for+Rust', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 1,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should filter by work model', async () => {
+      const res = await axios.get('/api/job-posts?model=on-site', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 5,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+
+      const res2 = await axios.get('/api/job-posts?model=remote', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res2.status).toBe(200);
+      expect(Array.isArray(res2.data.data)).toBe(true);
+      expect(res2.data.meta).toStrictEqual({
+        total: 2,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+
+      const res3 = await axios.get('/api/job-posts?model=hybrid', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res3.status).toBe(200);
+      expect(Array.isArray(res3.data.data)).toBe(true);
+      expect(res3.data.meta).toStrictEqual({
+        total: 2,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should filter by work models', async () => {
+      const res = await axios.get('/api/job-posts?model=on-site&model=hybrid', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 7,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should filter by min salary', async () => {
+      const res = await axios.get('/api/job-posts?salaryMin=80000', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 3,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should filter by max salary', async () => {
+      const res = await axios.get('/api/job-posts?salaryMax=55000', {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 2,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
+      });
+    });
+
+    it('should filter by salary range', async () => {
+      const res = await axios.get(
+        '/api/job-posts?salaryMax=80000&salaryMin=70000',
+        {
+          headers: { Authorization: `Bearer ${authToken}` },
+        }
+      );
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.data.data)).toBe(true);
+      expect(res.data.meta).toStrictEqual({
+        total: 3,
+        page: 1,
+        limit: 10,
+        totalPages: 1,
       });
     });
   });
--- docker/elastic/seed.sh
+++ docker/elastic/seed.sh
@@ -22,16 +22,61 @@ curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: applicati
   "description": "Backend developer position for Java Spring Boot applications",
   "id": "9495b509-1fa6-479a-ab28-3f706474a313",
   "salary": 70000,
-  "title": "Backend Developer",
+  "title": "Java Spring Boot Developer",
+  "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
+  "workModel": "on-site"
+}'
+
+curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
+  "description": "Backend developer position for Nodejs applications",
+  "id": "9495b509-1fa6-479a-ab28-3f706474a314",
+  "salary": 55000,
+  "title": "Nodejs Developer",
+  "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
+  "workModel": "on-site"
+}'
+
+curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
+  "description": "Backend developer position for GoLang applications",
+  "id": "9495b509-1fa6-479a-ab28-3f706474a315",
+  "salary": 65000,
+  "title": "Go Developer",
+  "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
+  "workModel": "on-site"
+}'
+
+curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
+  "description": "Backend developer position for Rust applications",
+  "id": "9495b509-1fa6-479a-ab28-3f706474a316",
+  "salary": 60000,
+  "title": "Rust Developer",
   "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
   "workModel": "on-site"
 }'
 
 curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
   "description": "Backend developer position for Java Spring Boot applications",
-  "id": "9495b509-1fa6-479a-ab28-3f706474a314",
-  "salary": 70000,
+  "id": "9495b509-1fa6-479a-ab28-3f706474a317",
+  "salary": 50000,
   "title": "Backend Developer",
   "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
   "workModel": "on-site"
+}'
+
+curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
+  "description": "Quality assurance",
+  "id": "9495b509-1fa6-479a-ab28-3f706474a318",
+  "salary": 80000,
+  "title": "QA Engineer",
+  "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
+  "workModel": "remote"
+}'
+
+curl -X POST "http://localhost:9200/job-posts/_doc/" -H "Content-Type: application/json" -d '{
+  "description": "Backend developer position for Ruby applications",
+  "id": "9495b509-1fa6-479a-ab28-3f706474a319",
+  "salary": 90000,
+  "title": "Backend Developer",
+  "userId": "3f2ecdea-02fe-48e1-bf7b-89ec6d028823",
+  "workModel": "hybrid"
 }'
